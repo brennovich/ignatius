99fade9ce4ca0f652460a30d61dce9a715308fbe
Bruno Henrique - Garu <squall.bruno@gmail.com>
2013-01-14 12:51:43 -0300

    improving message

--- lib/ignatius/cli.rb
+++ lib/ignatius/cli.rb
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 module Ignatius
   class Cli < Thor
-    desc "Create BLOG_PATH", "generate a new blog structure"
+    desc "new BLOG_PATH", "Generate a new blog structure"
     def new(path)
       generator = Generator.new
       generator.destination_root = path
