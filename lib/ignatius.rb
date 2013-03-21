1c4a2e2f1813d1b69deaa6c0b50e2cef332a0858
Brenno Costa <brennolncosta@gmail.com>
2013-03-21 01:13:58 -0300

    Add space

--- lib/ignatius.rb
+++ lib/ignatius.rb
@@ -3,7 +3,7 @@ require 'thor/group'
 require 'sinatra/base'
 require 'pathname'
 
-Dir[File.dirname(__FILE__) + '/ignatius/**/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/ignatius/**/*.rb'].each { |file| require file }
 
 module Ignatius
   $ROOT = File.dirname(__FILE__)
